05fdc10a63329d2bb7d95c3574d00d2c59017573
tarunutla <tarun.ut@tmachinesoftware.com>
2025-03-07 14:35:03 +0530

    Initial Hello World deployment

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -5,10 +5,11 @@ SECRET_KEY = 'django-insecure-t78d%5eitbv*%xgmwyw=l1_urmi0!h9b1qrw3yg8w$9v)m^n$%
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "127.0.0.1",
+    "127.0.0.1",  # Localhost for development
     "localhost",
-    "https://hello-world-j6hv.onrender.com",
+    "hello-world-j6hv.onrender.com",  # ✅ Add your Render domain here
 ]
+
 
 # ✅ Only allow specific origins instead of `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGINS = [
