16f71fc716f5f64fac5962bc90dcc1586a6f158c
tarunutla <tarun.ut@tmachinesoftware.com>
2025-03-07 14:57:31 +0530

    Fix root URL and update ALLOWED_HOSTS

--- mymessage/views.py
+++ mymessage/views.py
@@ -2,5 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 
+@api_view(['GET'])
 def send_hello(request):
-    return HttpResponse("Hello from the Django?!")
+    return Response({"message": "Hello from the Django!"})
