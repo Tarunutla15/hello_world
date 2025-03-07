af667530b00c410ae302bde5a757eb27cd22e56b
tarunutla <tarun.ut@tmachinesoftware.com>
2025-03-07 14:40:45 +0530

    Initial Hello World deployment

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from mymessage.views import send_hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', send_hello),
+    path('api/', include('mymessage.urls')),
 ]
